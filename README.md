0e26c1da665116b0ad6b500eeaaddc279f133f26
0xMayflower <0xMayflower@protonmail.com>
2021-07-03 00:13:19 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Work Hard Fork Template
+
+This repository is a tamplate repository to fork WHF easily.
+
+Here is the [example fork](#fork).
---
+++ hardhat.config.ts
@@ -0,0 +1,56 @@
+import { task } from 'hardhat/config'
+import '@nomiclabs/hardhat-waffle'
+import '@typechain/hardhat'
+import 'hardhat-dependency-compiler'
+import 'hardhat-gas-reporter'
+import 'solidity-coverage'
+
+import { HardhatUserConfig } from 'hardhat/types'
+
+require('dotenv').config()
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task('accounts', 'Prints the list of accounts', async (args, hre) => {
+  const accounts = await hre.ethers.getSigners()
+
+  for (const account of accounts) {
+    console.log(account.address)
+  }
+})
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.6',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 999999,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    hardhat: {
+      blockGasLimit: 9999999,
+      mining: {
+        auto: true,
+      },
+      forking: { url: process.env.FORK_URL },
+    },
+  },
+  typechain: {
+    outDir: 'src/types',
+    target: 'ethers-v5',
+    alwaysGenerateOverloads: false, // should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
+    externalArtifacts: ['externalArtifacts/*.json'], // optional array of glob patterns with external artifacts to process (for example external libs from node_modules)
+  },
+}
+
+export default config
