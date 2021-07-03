d56d9b1a70dba0f2296ca421c851cc05f4297c5c
0xMayflower <0xMayflower@protonmail.com>
2021-07-03 23:17:29 +0900

    increase timeout to 60s

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -54,6 +54,9 @@ const config: HardhatUserConfig = {
     alwaysGenerateOverloads: false, // should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
     externalArtifacts: ['externalArtifacts/*.json'], // optional array of glob patterns with external artifacts to process (for example external libs from node_modules)
   },
+  mocha: {
+    timeout: 60000,
+  },
 }
 
 export default config
